0ac4d7226a6b6c9e79c7bc3c00a140b6323f7b16
Amer <m1o1h1a1a1@gmail.com>
2026-08-22 15:32:17 +0300

    Revert "set up the project"
    This reverts commit b5003bff8b721dec58810febdad54146cf6bff78.

--- src/config/app.config.ts
+++
@@ -1,5 +0,0 @@
-export const appConfig = () => ({
-  port: Number(process.env.PORT ?? 3000),
-  frontendUrl: process.env.FRONTEND_URL ?? 'http://localhost:5173',
-  nodeEnv: process.env.NODE_ENV ?? 'development',
-});
--- src/shared/base.entity.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export abstract class BaseEntityColumns {
-  @CreateDateColumn()
-  createdAt!: Date;
-
-  @UpdateDateColumn()
-  updatedAt!: Date;
-}
